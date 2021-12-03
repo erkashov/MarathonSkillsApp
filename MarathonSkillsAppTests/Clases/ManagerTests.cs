8d81578751fecd5207d389119a986bc8503d8326
erkashov <emir2018k@yandex.ru>
2021-12-03 18:41:39 +0300

    123

---
+++ MarathonSkillsAppTests/Clases/ManagerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarathonSkillsApp.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarathonSkillsApp.Clases.Tests
+{
+    [TestClass()]
+    public class ManagerTests
+    {
+        [TestMethod()]
+        public void GetCategoryTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
